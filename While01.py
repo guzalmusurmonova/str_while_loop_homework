0b521299a2b088bdd019df574021ad1df37402bd
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-04-24 21:35:45 -0700

    01

--- While01.py
+++ While01.py
@@ -5,5 +5,14 @@ def main(s):
         s: str
     Returns:
         int: return answer
-    """
-    return
+    """ 
+    n=0
+    i=0  
+    while n<len(s): 
+        if s[n].isdigit():
+            i=i+1 
+        n=n+1  
+
+        
+    return i 
+print(main("python 2022"))
