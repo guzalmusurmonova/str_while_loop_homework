887f3a4e06f151520472c5ef12feffd2c8bce086
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-04-25 00:21:44 -0700

    06

--- While06.py
+++ While06.py
@@ -6,4 +6,31 @@ def main(s):
         consonant: other than vowels(a, e, i, o, u)
     Returns:
         int: return answer
-    """
+    """ 
+    n=0
+    i=0  
+    while n<len(s): 
+        if s[n]=="a":
+            i=i+1 
+    
+        if s[n]=="e":
+            i=i+1 
+     
+        if s[n]=="i":
+            i=i+1 
+    
+        if s[n]=="u":
+            i=i+1 
+    
+        if s[n]=="o'":
+            i=i+1 
+    
+        if s[n]=="o":
+
+            i=i+1 
+        n=n+1  
+    return i 
+print(main("CodeschoolUz"))
+   
+
+        
