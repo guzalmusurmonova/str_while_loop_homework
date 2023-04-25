315ec3927c60c0c89cf583b04ba64ec13d2b29fa
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-04-25 00:53:55 -0700

    07

--- While07.py
+++ While07.py
@@ -5,5 +5,13 @@ def main(s):
         s: str
     Returns:
         int: return answer
-    """
-    return
+    """ 
+    n=0
+    i=0  
+    while n<len(s): 
+        if int(s[n])%2==0:
+            i=i+1 
+        n=n+1  
+
+    return i 
+print(main("56786543250"))
